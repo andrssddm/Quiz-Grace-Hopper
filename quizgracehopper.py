d1c4858d5aa36152f845d18207e6b74636013d71
andrssddm <andressa.duda@academico.ifpb.edu.br>
2026-03-20 10:09:57 -0300

    quizgracehopper.py

--- quizgracehopper.py
+++ quizgracehopper.py
@@ -123,7 +123,6 @@ elif st.session_state.pagina == "quiz":
             st.session_state.acertou = False
             if st.session_state.q_atual >= len(questoes):
                 st.session_state.pagina = "resultado"
-            st.experimental_rerun()
 
 # ---------------- RESULTADO FINAL ----------------
 elif st.session_state.pagina == "resultado":
