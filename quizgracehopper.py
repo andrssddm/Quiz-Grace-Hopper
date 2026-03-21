b9b144e296aa6cc03b36262af378645ba654bb59
Andressa Duda <andressa.duda@academico.ifpb.edu.br>
2026-03-21 13:30:41 -0300

    quizgracehopper.py

--- quizgracehopper.py
+++ quizgracehopper.py
@@ -179,9 +179,9 @@ elif st.session_state.pagina == "quiz":
     q = questoes[st.session_state.q_atual]
 
     st.markdown(
-        f"<h2 style='font-family: Playfair Display; color:#C43670;'>⤷ Questão {st.session_state.q_atual + 1}</h2>",
-        unsafe_allow_html=True
-    )
+    f"<h2 style='font-family: Playfair Display; color:#C43670;'>⤷ Questão {st.session_state.q_atual + 1}</h2>",
+    unsafe_allow_html=True
+)
 
     st.markdown(
         f"<h3 style='color:#C43670;'>{q['pergunta']}</h3>",
